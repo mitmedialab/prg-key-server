5844da0fbd96a9344ab63ba43821cef711f65953
p-buddy <pmalacho@usc.edu>
2023-01-24 17:32:32 -0500

    Initial commit. Not yet sure about CORS or env variables

---
+++ netlify/functions/ai-blocks/drive.ts
@@ -0,0 +1,30 @@
+import { Handler, HandlerEvent } from "@netlify/functions";
+
+export const handler: Handler = async ({ headers: requestHeaders }: HandlerEvent) => {
+
+  const blocksDomain = "https://playground.raise.mit.edu/";
+  const { Origin } = requestHeaders;
+
+  const headers = {
+    'Access-Control-Allow-Origin': blocksDomain,
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET'
+  };
+
+  return Origin?.startsWith(blocksDomain)
+    ? {
+      statusCode: 200,
+      body: JSON.stringify({
+        appId: process.env.DRIVE_APP_ID,
+        clientId: process.env.DRIVE_CLIENT_ID,
+        developerKey: process.env.DEVELOPER_KEY,
+      }),
+      headers
+    }
+    : {
+      statusCode: 400,
+      body: "Bad request to key server. If developing locally, opt to use your own, local keys.",
+      headers
+    }
+};
+
