b545b68a33e9a58f1067e90d2833b4a5648f0355
p-buddy <pmalacho@usc.edu>
2023-01-25 12:45:28 -0500

    Fixing up CORS

--- netlify/functions/ai-blocks-drive.ts
+++ netlify/functions/ai-blocks-drive.ts
@@ -5,26 +5,28 @@ const handler: Handler = async ({ headers: requestHeaders }: HandlerEvent) => {
   const blocksDomain = "https://playground.raise.mit.edu/";
   const { Origin } = requestHeaders;
 
-  const headers = {
-    'access-control-allow-origin': blocksDomain,
-    'Access-Control-Allow-Methods': 'GET'
+  const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'GET',
   };
 
-  headers['access-control-allow-origin'] = "*";
+  const headers = { ...CORS, 'Content-Type': 'application/json' };
 
   return Origin?.startsWith(blocksDomain)
     ? {
+      headers,
       statusCode: 200,
       body: JSON.stringify({
         appId: process.env.DRIVE_APP_ID,
         clientId: process.env.DRIVE_CLIENT_ID,
         developerKey: process.env.DEVELOPER_KEY,
       }),
-      headers
     }
     : {
+      headers,
       statusCode: 400,
-      body: "Bad request to key server. If developing locally, opt to use your own, local keys.",
+      body: JSON.stringify({ error: "Bad request to key server. If developing locally, opt to use your own, local keys." }),
     }
 };
 
