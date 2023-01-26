01e3ba153f2507ba011f68cea3bc49d3d40e0680
p-buddy <pmalacho@usc.edu>
2023-01-25 19:44:13 -0500

    Removing files that should not be committed

--- endpoints/ai-blocks/bundle.d.ts
+++
@@ -1,6 +0,0 @@
-declare type URLParameters = {
-    session: string;
-};
-declare function export_default(params: URLParameters): Promise<Record<string, string>>;
-
-export { URLParameters, export_default as default };
